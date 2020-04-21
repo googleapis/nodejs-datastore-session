0f65e29c9fe1eba4fef0387e11ca7dfe42ece713
Justin Beckwith <beckwith@google.com>
2020-04-21 16:05:49 -0700

    bugs

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2017 Google LLC
+// Copyright 2020 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// eslint-disable-next-line node/no-unpublished-import
 import * as expressSession from 'express-session';
 import {Datastore} from '@google-cloud/datastore';
 import {
